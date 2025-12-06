fd623d6a536e9e93cddabc4cf09c223170245830
Jessie Edmisten <jaedmisten@gmail.com>
2025-12-06 08:13:07 -0500

    first commit

---
+++ content.js
@@ -0,0 +1,5 @@
+console.log("%cHELLO WORLD!!!", "color:blue;font-size: 15px;");
+let logoEl = document.getElementById("logo");
+console.log(logoEl);
+console.dir(logoEl);
+logoEl.innerHTML = '<a href="https://www.youtube.com">YouTube</a>';
